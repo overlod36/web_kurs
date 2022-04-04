3b0d1a45071b1ced2a266512116b7bfbf8ec52a4
kirbi <Biehan@mail.ru>
2022-04-05 02:09:04 +0300

    some shit

--- scripts/root.js
+++ scripts/root.js
@@ -1,2 +1,5 @@
 const ROOT_rock90_cas = document.getElementById('rock90_cas_list');
 const ROOT_rap_cas = document.getElementById('rap_cas');
+const ROOT_blues_cas = document.getElementById('blues_cas');
+const ROOT_el_cas = document.getElementById('el_cas');
+const ROOT_rock00_dvd = document.getElementById('rock00_dvd');
